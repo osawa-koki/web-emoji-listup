59353132eaef0382c238201cf3f29842fbedce84
osawa-koki <koki@osawa.dev>
2023-12-16 12:36:23 +0900

    small fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
-import setting from '../setting'
 import { Card } from 'react-bootstrap'
 import { toast } from 'react-toastify'
+import setting from '../setting'
 
 interface Emoji {
   char: string
